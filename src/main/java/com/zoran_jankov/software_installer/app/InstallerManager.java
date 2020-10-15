15ad91d1b2935656302f744ed74e74a938a51b8d
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-10-15 14:36:39 +0200

    Download installers from network and from the internet implementation

--- src/main/java/com/zoran_jankov/software_installer/app/InstallerManager.java
+++ src/main/java/com/zoran_jankov/software_installer/app/InstallerManager.java
@@ -1,8 +1,13 @@
 package com.zoran_jankov.software_installer.app;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import org.apache.commons.io.FileUtils;
 
 public class InstallerManager
 {
@@ -29,11 +34,45 @@ public class InstallerManager
 		}
 	}
 
-	public void download(String software)
+	public void downloadFromInternet(String application)
 	{
-		
+		File destination = new File(settings.getLocalPath(application));
+		URL source = null;
+
+		try
+		{
+			source = new URL(settings.getDownloadURL(application));
+		}
+		catch (MalformedURLException e)
+		{
+			e.printStackTrace();
+		}
+
+		try
+		{
+			FileUtils.copyURLToFile(source, destination);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
+	public void downloadFromNetwork(String application)
+	{
+		File source = new File(settings.getNetworkPath(application));
+		File destination = new File(settings.getLocalPath(application));
+
+		try
+		{
+			FileUtils.copyFile(source, destination);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
 	public void startInstallation(Map<String, Boolean> softwareToInstall)
 	{
 		for (Entry<String, Boolean> application : softwareToInstall.entrySet())
--- src/main/java/com/zoran_jankov/software_installer/controller/MainWindowController.java
+++ src/main/java/com/zoran_jankov/software_installer/controller/MainWindowController.java
@@ -2,10 +2,17 @@ package com.zoran_jankov.software_installer.controller;
 
 import com.zoran_jankov.software_installer.app.InstallerManager;
 import com.zoran_jankov.software_installer.app.InstallerSettings;
+import com.zoran_jankov.software_installer.app.Software;
 
 public class MainWindowController
 {
-    private static final String INSTALLER_SETTINGS_PATH = "/Installer-Settings.ini";
+    private static final String INSTALLER_SETTINGS_PATH = "/Installers-Settings.ini";
     private InstallerSettings installerSettings = new InstallerSettings(INSTALLER_SETTINGS_PATH);
     private InstallerManager installerManager = new InstallerManager(installerSettings);
+    
+    public MainWindowController()
+    {
+        System.out.println(Software.CCLEANER);
+        installerManager.downloadFromInternet(Software.CCLEANER);
+    }
 }
