fbf1132a62b857b949d712291e0fb8227eca9007
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-10-19 00:31:43 +0200

    Update SelectAppFactoryUtils.java

--- src/main/java/com/zoran_jankov/software_installer/gui/SelectAppFactoryUtils.java
+++ src/main/java/com/zoran_jankov/software_installer/gui/SelectAppFactoryUtils.java
@@ -7,14 +7,14 @@ import javax.swing.JCheckBox;
 
 import com.zoran_jankov.software_installer.app.InstallerSettings;
 
-public class SelectAppFactory
+public class SelectAppFactoryUtils
 {
 	public static InstallerSettings settings = InstallerSettings.getInstance();
 	
 	public static JCheckBox getInstance(String application)
 	{
 		JCheckBox selectApp = new JCheckBox(application);
-		URL iconURL = SelectAppFactory.class.getClass().getResource(settings.getIconPath(application));
+		URL iconURL = SelectAppFactoryUtils.class.getClass().getResource(settings.getIconPath(application));
 		ImageIcon icon = new ImageIcon(iconURL);
 		selectApp.setIcon(icon);
 		 
